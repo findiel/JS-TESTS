4a8266ca068811797766a0502a79a50556a2664a
Fjollsfinn <bartlomiejtuchowski@gmail.com>
2018-08-06 13:48:34 +0200

    update

--- specs/first-test.js
+++ specs/first-test.js
@@ -1,11 +1,9 @@
-var assert = require('assert');
-
-browser.timeouts('pageLoad', 30000); //Use this command in tests if your connection SUCKS.
+let assert = require('assert');
 
 describe('webdriver.io page', function() {
     it('should have the right title - the fancy generator way', function () {
         browser.url('http://webdriver.io');
-        var title = browser.getTitle();
+        let title = browser.getTitle();
         console.log(globals);
         assert.equal(title, 'WebdriverIO - WebDriver bindings for Node.js');
     });
